6b3321287997bbc01e61f2ce48d61c835c845cb6
Katelyn Rohrer <k.rohrer03@gmail.com>
2023-02-15 19:43:46 -0700

    Added file search and export

---
+++ export_graphs.py
@@ -0,0 +1,34 @@
+
+import glob
+import os
+import graph_single
+
+
+def main():
+
+    # find_files()
+    export("/Users/katierohrer/Desktop/MOCA/GitHub/All-Lab-Data/Lab/Experiment3_ShoulderAbduction/ES3F/Trial1_Slow/ShoulderAA.Forearm.ES3F.Run1.Biostamp.7_27_22.Slow.angularDisp.csv")
+
+def find_files(search_term=""):
+    current_dir = os.getcwd()
+    parent_dir = current_dir[:current_dir.rindex(os.sep)]
+    csv_path = os.path.join(parent_dir, "All-Lab-Data", "Lab")
+    files = glob.glob(csv_path + os.sep + "**" + os.sep + f"*{search_term}*angularDisp.csv", recursive=True)
+    print(files)
+
+def export(chosen_file):
+    csv_file_name = chosen_file[chosen_file.rindex(os.sep)+1:-4]
+    file_info = csv_file_name.split(".")
+    file_info = [item for item in file_info if item != ""]
+    motion = file_info[0].lower()
+    subject = file_info[2].lower()
+
+    save_file_name = csv_file_name + ".png"
+
+
+    abs_path = os.path.abspath("")[:os.path.abspath("").rindex(os.sep)]
+    save_path = os.path.join(abs_path, "All-Lab-Data", "all_images", motion, subject, save_file_name)
+    plt.savefig(save_path, bbox_inches='tight', pad_inches=0.5)
+
+if __name__ == "__main__":
+    main()
